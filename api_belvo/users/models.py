d4c72742ea284a8cf2109d05dd1c610f663a2dab
Alejandro <jose.alejandro.her.ros@gmail.com>
2020-04-14 23:29:03 +0200

    Improve user model and exclude flake8 migrations

--- api_belvo/users/models.py
+++ api_belvo/users/models.py
@@ -1,3 +1,25 @@
 from django.db import models  # NOQA
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from core.models.time_stamped import TimeStampedModel
+
+
+class User(TimeStampedModel):
+    """User model."""
+
+    name = models.TextField(
+        _('name'),
+        help_text=_('Name for user'),
+        null=True, blank=True
+    )
+    email = models.EmailField(
+        _('email address'),
+        blank=True,
+        unique=True,
+        help_text=_('I stand out in that an e-mail is stored')
+    )
+    age = models.IntegerField(
+        _('age'),
+        default=0,
+        help_text=_('Age of users')
+    )
