2437fa6845ebe57f921954aa33aacf953828246a
Alejandro <jose.alejandro.her.ros@gmail.com>
2020-04-23 01:35:14 +0200

    Improve urls for users

--- api_belvo/users/urls.py
+++ api_belvo/users/urls.py
@@ -7,12 +7,12 @@ app_name = 'users'
 urlpatterns = [
     url(
         r'^$',
-        views.UserListCreate.as_view(),
+        views.UserListCreateViewSet.as_view(),
         name='list_create_users'
     ),
     url(
         r'^(?P<user_id>[-.\w]+)/transactions$',
-        views.UserTransaction.as_view(),
+        views.UserTransactionViewSet.as_view(),
         name='user_transaction'
     ),
 ]
