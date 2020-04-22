310927d4c895464d4c71f44138561ace9db17217
Alejandro <jose.alejandro.her.ros@gmail.com>
2020-04-23 01:34:46 +0200

    Improve views for users

--- api_belvo/users/views.py
+++ api_belvo/users/views.py
@@ -4,29 +4,30 @@ from rest_framework import generics
 from rest_framework.response import Response
 
 from transactions.models import Transaction
-from transactions.serializers import TransactionSerializer
+from transactions.serializers import ListUserTransactionSerializer
 from users.models import User
-from users.serializers import UserSerializer, UserTransactionSerializer
+from users.serializers import UserSerializer
 
 logger = logging.getLogger(__name__)
 
 
-class UserListCreate(generics.ListCreateAPIView):
+class UserListCreateViewSet(generics.ListCreateAPIView):
     """USer Crear and list user."""
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
-class UserTransaction(generics.ListCreateAPIView):
+class UserTransactionViewSet(generics.ListAPIView):
     """USer Crear and list user."""
 
     queryset = Transaction.objects.all()
-    serializer_class = TransactionSerializer
-    get_serializer_list_class = UserTransactionSerializer
+    serializer_class = ListUserTransactionSerializer
 
     def list(self, request, *args, **kwargs):
         """List users transactions."""
-        user = User.objects.get(id=kwargs.get('user_id'))
-        serializer = UserTransactionSerializer(user)
+        queryset = Transaction.objects.filter(
+            user_id=kwargs.get('user_id')
+        )
+        serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
