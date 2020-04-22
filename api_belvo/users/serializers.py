3e798bb7cfbb81fdc39a31134090193a8d327b8b
Alejandro <jose.alejandro.her.ros@gmail.com>
2020-04-23 01:33:39 +0200

    Improve users serializers

--- api_belvo/users/serializers.py
+++ api_belvo/users/serializers.py
@@ -1,15 +1,17 @@
 from rest_framework import serializers
 
-from transactions.serializers import TransactionSerializer
-
 from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
     """User Serializer."""
 
-    name = serializers.CharField()
-    email = serializers.EmailField()
+    name = serializers.CharField(
+        required=True
+    )
+    email = serializers.EmailField(
+        required=True
+    )
     age = serializers.IntegerField(
         min_value=0,
         max_value=99
@@ -20,15 +22,3 @@ class UserSerializer(serializers.ModelSerializer):
 
         model = User
         fields = ('name', 'email', 'age', )
-
-
-class UserTransactionSerializer(serializers.ModelSerializer):
-    """User Serializer."""
-
-    transactions = TransactionSerializer(many=True)
-
-    class Meta:
-        """Meta class for User Serializer."""
-
-        model = User
-        fields = ('name', 'email', 'age', 'transactions')
